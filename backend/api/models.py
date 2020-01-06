1f74a7ac11068e43d212a919feb5d1bd3e164d7d
jordan <jrimmer95@gmail.com>
2020-01-06 21:54:36 +0000

    Creating django base in backend requirements.txt created from pip freeze

---
+++ backend/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from user.models import User
+
+# Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    url = models.URLField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+
+class Transaction(models.Model):
+    transaction_id = models.AutoField(primary_key=True)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
---
+++ backend/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('/', include(router.urls))
+]
